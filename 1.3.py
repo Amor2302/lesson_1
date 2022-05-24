6f2a772e75fa04a63886e32cf13e654b1ac997e2
Roman Radzhabov <reradzhabov@yandex.ru>
2022-05-24 12:14:46 +0300

    добавил

--- 1.3.py
+++
@@ -1,6 +0,0 @@
-number = list(range(1, 101, 1))
-number = 0
-
-
-if number >= 5 and number <= 20:
-    print(number)
---
+++ lesson_1/1.2.py
@@ -0,0 +1,10 @@
+num = 0
+cub_num = 0
+sum_cub_num = 0
+
+while num <= 1000:
+    if num % 2 != 0:
+        print(num)
+    num += 1
+
+
