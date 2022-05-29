aa3238d901353101e717e54f1d04e32f490eafdd
Roman <105938135+Amor2302@users.noreply.github.com>
2022-05-29 11:45:06 +0300

    доделал

--- 1.3.py
+++ 1.3.py
@@ -3,11 +3,11 @@ num = 0
 for num in range(1, 101):
     if 9 < num < 21:
         print(num, 'процентов')
+    elif 1 < num % 10 < 5:
+        print(num, 'процента')
     elif num % 10 == 0:
         print(num, 'процентов')
     elif num % 10 == 1:
         print(num, 'процент')
-    elif 1 < num % 10 < 5:
-        print(num, 'процента')
     else:
         print(num, 'процентов')
---
+++ проверка.py
@@ -0,0 +1,13 @@
+cub_num = 1234
+sum_cub_num = 0
+index = int
+if index(cub_num)[0]:
+
+    else sum_cub_num =
+
+
+
+#while cub_num != 0:
+    sum_cub_num += cub_num % 10
+    cub_num //= 10
+    print(sum_cub_num)
