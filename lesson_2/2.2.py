2e1f35f4d43c2ccbecb879a2b4dfa498d93fac4b
Roman <105938135+Amor2302@users.noreply.github.com>
2022-05-30 20:46:55 +0300

    добавил

--- lesson_2/2.2.py
+++ lesson_2/2.2.py
@@ -1,23 +1,13 @@
-mes = 0
-
 l_t = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
-message = ''
+
 i = 0
 
 while i < len(l_t):
-    if l_t[i].isdigit():
-        l_t[i] = l_t[i].zfill(2)
+    if l_t[i].isnumeric():
+        l_t[i] = '"' + l_t[i].zfill(2) + '"'
     elif l_t[i].startswith('+'):
-        l_t[i] = l_t[i].zfill(3)
+        l_t[i] = '"' + l_t[i].zfill(3) + '"'
     i += 1
-    print(l_t)
-# n = 1
-# while n < len(l_t):
-#     if l_t[n].isdigit():
-#         l_t.insert(n, '"')
-#         l_t.insert(n + 2, '"')
-#     n +=2
-# f = 0
-#
-#
-# print(l_t)
+l_t = ''.join(l_t)
+
+print(l_t)
