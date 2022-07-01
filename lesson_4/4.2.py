63a7747da9425a3ecae53b1a6f944f161b4b36bd
Roman <105938135+Amor2302@users.noreply.github.com>
2022-07-01 11:53:45 +0300

    залил

--- lesson_4/4.2.py
+++ lesson_4/4.2.py
@@ -6,7 +6,9 @@ response = requests.get(URL)
 
 data = response.text.split('</Valute>')
 
-money_code_lst = [i[(i.find('<CharCode>') + len('<CharCode>')): i.find('</CharCode>')] for i in data]
+money_code_lst = i[(i.find('<CharCode>') + len('<CharCode>')): i.find('</CharCode>')]
+for i in money_code_lst:
+
 nominal_lst = [i[(i.find('<Nominal>') + len('<Nominal>')): i.find('</Nominal>')] for i in data]
 money_name_lst = [i[(i.find('<Name>') + len('<Name>')): i.find('</Name>')] for i in data]
 value_lst = [i[(i.find('<Value>') + len('<Value>')): i.find('</Value>')].replace(',', '.') for i in data]
